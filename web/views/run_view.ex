92b06ce0044e2d0749373b96abd64a9617eb965e
Drew Seward <awswrd@gmail.com>
2016-12-10 23:47:11 -0800

    Handle no runs case on run index

--- web/views/run_view.ex
+++ web/views/run_view.ex
@@ -2,6 +2,10 @@ defmodule Rundot.RunView do
   use Rundot.Web, :view
   use Timex
 
+  def new_run_link(conn, label \\ "New run") do
+    link(label, to: run_path(conn, :new))
+  end
+
   def get_today_as_date do
     t = Timex.local |> Timex.to_date |> Timex.to_datetime(:local)
     %Ecto.DateTime{year: t.year, month: t.month, day: t.day}
