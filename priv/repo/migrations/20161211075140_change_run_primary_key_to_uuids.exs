7178400078c01929a4d7f86dedd09f35c84b32c5
Drew Seward <awswrd@gmail.com>
2016-12-11 14:34:05 -0800

    Just blow away the data
    I don't have any that really matters yet anyway, so it's fine.

--- priv/repo/migrations/20161211075140_change_run_primary_key_to_uuids.exs
+++ priv/repo/migrations/20161211075140_change_run_primary_key_to_uuids.exs
@@ -1,13 +1,33 @@
 defmodule Rundot.Repo.Migrations.ChangeRunPrimaryKeyToUuids do
   use Ecto.Migration
 
-  def change do
-    alter table(:runs) do
-      remove :id
-    end
+  # Unfortunately, this migration loses all data in the runs table
+  # going both ways...
 
-    alter table(:runs) do
+  def up do
+    drop table(:runs)
+
+    create table(:runs, primary_key: false) do
       add :id, :uuid, primary_key: true
+      add :start_time, :datetime
+      add :end_time, :datetime
+      add :distance, :float
+      add :distance_unit, :integer, null: false, default: 0
+
+      timestamps()
+    end
+  end
+
+  def down do
+    drop table(:runs)
+
+    create table(:runs) do
+      add :start_time, :datetime
+      add :end_time, :datetime
+      add :distance, :float
+      add :distance_unit, :integer, null: false, default: 0
+
+      timestamps()
     end
   end
 end
